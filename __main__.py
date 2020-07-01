2906ba0b07919dcc636b516ff7ee7df374859c28
scottcwang <wangsc@cs.wisc.edu>
2020-07-01 16:49:36 +0000

    Support packing key dicts into files

--- __main__.py
+++ __main__.py
@@ -10,6 +10,7 @@ import struct
 import warnings
 import pprint
 import abc
+import secrets
 
 import bcrypt
 import cryptography.hazmat.primitives.ciphers as ciphers
@@ -19,6 +20,7 @@ from cryptography.hazmat.backends import default_backend
 
 
 OPENSSH_DEFAULT_STRING_LENGTH_SIZE = 4
+WRAP_COL = 70
 
 
 class PascalStyleFormatInstruction(enum.Enum):
@@ -76,6 +78,53 @@ class PascalStyleByteStream(io.BytesIO):
         )
         return self.read_fixed_bytes(length)
 
+    def write_from_format_instruction(
+        self,
+        format_instruction,
+        value,
+        string_length_size=OPENSSH_DEFAULT_STRING_LENGTH_SIZE
+    ):
+        write_bytes = None
+        if isinstance(format_instruction, str):
+            write_bytes = struct.pack(format_instruction, value)
+        elif isinstance(format_instruction, PascalStyleFormatInstruction):
+            if format_instruction == PascalStyleFormatInstruction.BYTES:
+                assert isinstance(value, bytes)
+                write_bytes = value
+            elif format_instruction == PascalStyleFormatInstruction.STRING:
+                assert isinstance(value, str)
+                write_bytes = value.encode()
+            elif format_instruction == PascalStyleFormatInstruction.MPINT:
+                assert isinstance(value, int)
+                write_bytes = value.to_bytes(
+                    length=value.bit_length(),
+                    byteorder='big',
+                    signed=True
+                )
+                if value > 0 and write_bytes[0] // 128 == 1:
+                    write_bytes = b'\x00' + write_bytes
+            write_bytes_len_bytes = len(write_bytes).to_bytes(
+                length=OPENSSH_DEFAULT_STRING_LENGTH_SIZE,
+                byteorder='big',
+                signed=False
+            )
+            write_bytes = write_bytes_len_bytes + write_bytes
+        if write_bytes is None:
+            raise NotImplementedError()
+        self.write(write_bytes)
+        return
+
+    def write_from_format_instructions_dict(
+        self,
+        format_instructions_dict,
+        values_dict
+    ):
+        for k, v in format_instructions_dict.items():
+            self.write_from_format_instruction(
+                v,
+                values_dict[k]
+            )
+
 
 class KDF(abc.ABC):
     @staticmethod
@@ -126,17 +175,36 @@ _kdf = {
 class Cipher(abc.ABC):
     @staticmethod
     @abc.abstractmethod
+    def encrypt(cipher_key, initialization_vector, cipher_bytes):
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
     def decrypt(cipher_key, initialization_vector, cipher_bytes):
         pass
 
 
 class NoneCipher(Cipher):
     @staticmethod
+    def encrypt(cipher_key, initialization_vector, cipher_bytes):
+        return cipher_bytes
+
+    @staticmethod
     def decrypt(cipher_key, initialization_vector, cipher_bytes):
         return cipher_bytes
 
 
 class AES_GCM(Cipher):
+    @staticmethod
+    def encrypt(cipher_key, initialization_vector, cipher_bytes):
+        cipher = ciphers.Cipher(
+            algorithms.AES(cipher_key),
+            modes.CTR(initialization_vector),
+            backend=default_backend()
+        )
+        encryptor = cipher.encryptor()
+        return encryptor.update(cipher_bytes) + encryptor.finalize()
+
     @staticmethod
     def decrypt(cipher_key, initialization_vector, cipher_bytes):
         cipher = ciphers.Cipher(
@@ -157,7 +225,7 @@ _cipher = {
 _clear_bytes_header = {
     'auth_magic': '15s',
     'cipher': PascalStyleFormatInstruction.STRING,
-    'kdf_name': PascalStyleFormatInstruction.STRING,
+    'kdf': PascalStyleFormatInstruction.STRING,
     'kdf_options': PascalStyleFormatInstruction.BYTES,
     'num_keys': '>i'
 }
@@ -177,8 +245,8 @@ _public_key = {
 }
 
 _cipher_bytes_header = {
-    'checkint1': '>i',
-    'checkint2': '>i'
+    'check_int_1': '>I',
+    'check_int_2': '>I'
 }
 
 _private_key = {
@@ -267,12 +335,12 @@ def parse_private_bytes(private_bytes):
     private_bytes = byte_stream.read_from_format_instruction(
         PascalStyleFormatInstruction.BYTES)
 
-    if key['kdf_name'] != 'none':
+    if key['kdf'] != 'none':
         passphrase = getpass.getpass('Key passphrase: ')
     else:
         passphrase = ''
 
-    if key['kdf_name'] not in _kdf:
+    if key['kdf'] not in _kdf:
         warnings.warn('Unsupported KDF type')
         key['private_bytes'] = private_bytes
         return key
@@ -280,9 +348,9 @@ def parse_private_bytes(private_bytes):
     key['kdf_options'] = PascalStyleByteStream(
         key['kdf_options']
     ).read_from_format_instructions_dict(
-        _kdf[key['kdf_name']]['options_format']
+        _kdf[key['kdf']]['options_format']
     )
-    kdf_result = _kdf[key['kdf_name']]['kdf'].derive_key(
+    kdf_result = _kdf[key['kdf']]['kdf'].derive_key(
         key['kdf_options'], passphrase)
 
     if key['cipher'] not in _cipher:
@@ -300,7 +368,7 @@ def parse_private_bytes(private_bytes):
 
     key['cipher_header'] = cipher_byte_stream.read_from_format_instructions_dict(
         _cipher_bytes_header)
-    if key['cipher_header']['checkint1'] != key['cipher_header']['checkint2']:
+    if key['cipher_header']['check_int_1'] != key['cipher_header']['check_int_2']:
         warnings.warn('Cipher header check numbers do not match')
         key['private_bytes'] = private_bytes
     else:
@@ -332,6 +400,135 @@ def parse_private_bytes(private_bytes):
             warnings.warn('Incorrect padding at end of ciphertext')
 
     return key
+
+
+def pack_public_text_from_public_dict(filename, key_dict):
+    text_stream = io.StringIO('')
+    for key in key_dict['keys']:
+        text_stream.write(key['public']['key_type'] + ' ')
+        public_key_bytes = pack_public_key(key['public'])
+        public_keys_b64 = base64.b64encode(public_key_bytes).decode()
+        text_stream.write(public_keys_b64 + ' ')
+        text_stream.write(key['comment'])
+        text_stream.write('\n')
+    with open(filename, 'w') as fd:
+        fd.write(text_stream.getvalue())
+
+
+def pack_public_text_from_private_dict(filename, key_dict):
+    text_stream = io.StringIO('')
+    for key in key_dict['keys']:
+        text_stream.write(key['private']['key_type'] + ' ')
+        public_key_bytes = pack_public_key(key['private'])
+        public_keys_b64 = base64.b64encode(public_key_bytes).decode()
+        text_stream.write(public_keys_b64 + ' ')
+        text_stream.write(key['private']['footer']['comment'])
+        text_stream.write('\n')
+    with open(filename, 'w') as fd:
+        fd.write(text_stream.getvalue())
+
+
+def pack_private_text(filename, key_dict, cipher='none', kdf='none', kdf_options={'': b''}):
+    text_stream = io.StringIO('')
+    text_stream.write('-----BEGIN OPENSSH PRIVATE KEY-----\n')
+    private_keys_bytes = pack_private_keys(
+        key_dict['keys'], cipher, kdf, kdf_options)
+    private_keys_b64 = base64.b64encode(private_keys_bytes).decode()
+    private_keys_wrapped = ''.join([
+        (private_keys_b64[i: min(i + WRAP_COL, len(private_keys_b64))] + '\n')
+        for i in range(0, len(private_keys_b64), WRAP_COL)
+    ])
+    text_stream.write(private_keys_wrapped)
+    text_stream.write('-----END OPENSSH PRIVATE KEY-----\n')
+    with open(filename, 'w') as fd:
+        fd.write(text_stream.getvalue())
+
+
+def pack_public_key(public_key):
+    byte_stream = PascalStyleByteStream(b'')
+
+    byte_stream.write_from_format_instructions_dict(
+        _key_header, public_key
+    )
+    byte_stream.write_from_format_instructions_dict(
+        _public_key[public_key['key_type']], public_key['values']
+    )
+
+    return byte_stream.getvalue()
+
+
+def pack_private_keys(private_keys, cipher='none', kdf='none', kdf_options={'': b''}):
+    if cipher not in _cipher:
+        raise NotImplementedError('Unsupported cipher')
+    if kdf not in _kdf:
+        raise NotImplementedError('Unsupported KDF')
+
+    kdf_options_byte_stream = PascalStyleByteStream(b'')
+    kdf_options_byte_stream.write_from_format_instructions_dict(
+        _kdf[kdf]['options_format'], kdf_options
+    )
+
+    byte_stream = PascalStyleByteStream(b'')
+    clear_bytes_header = {
+        'auth_magic': b'openssh-key-v1\x00',
+        'cipher': cipher,
+        'kdf': kdf,
+        'kdf_options': kdf_options_byte_stream.getvalue(),
+        'num_keys': len(private_keys)
+    }
+    byte_stream.write_from_format_instructions_dict(
+        _clear_bytes_header, clear_bytes_header
+    )
+
+    cipher_byte_stream = PascalStyleByteStream(b'')
+    check_int = secrets.randbits(32)
+    cipher_bytes_header = {
+        'check_int_1': check_int,
+        'check_int_2': check_int
+    }
+    cipher_byte_stream.write_from_format_instructions_dict(
+        _cipher_bytes_header, cipher_bytes_header)
+
+    for key in private_keys:  # openssh only supports one key at a time: https://github.com/openssh/openssh-portable/blob/e073106f370cdd2679e41f6f55a37b491f0e82fe/sshkey.c#L4067
+        byte_stream.write_from_format_instruction(
+            PascalStyleFormatInstruction.BYTES,
+            pack_public_key(key['private'])
+        )
+
+        cipher_byte_stream.write_from_format_instructions_dict(
+            _key_header,
+            key['private']
+        )
+        cipher_byte_stream.write_from_format_instructions_dict(
+            _private_key[key['private']['key_type']],
+            key['private']['values']
+        )
+        cipher_byte_stream.write_from_format_instructions_dict(
+            _private_key_footer,
+            key['private']['footer']
+        )
+
+    if kdf != 'none':
+        passphrase = getpass.getpass('Key passphrase: ')
+    else:
+        passphrase = ''
+    kdf_result = _kdf[kdf]['kdf'].derive_key(kdf_options, passphrase)
+
+    cipher_bytes = cipher_byte_stream.getvalue()
+    cipher_bytes = cipher_bytes + \
+        (b'\x01\x02\x03\x04\x05\x06\x07\x08')[
+            :(-len(cipher_bytes)) % 8]  # TODO Pad to cipher-appropriate block size: https://github.com/openssh/openssh-portable/blob/90e51d672711c19a36573be1785caf35019ae7a8/cipher-aesctr.h#L23
+    cipher_bytes_encrypted = _cipher[cipher].encrypt(
+        kdf_result['cipher_key'],
+        kdf_result['initialization_vector'],
+        cipher_bytes
+    )
+    byte_stream.write_from_format_instruction(
+        PascalStyleFormatInstruction.BYTES,
+        cipher_bytes_encrypted
+    )
+
+    return byte_stream.getvalue()
 
 
 if __name__ == "__main__":
