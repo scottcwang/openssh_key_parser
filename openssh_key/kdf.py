53d8466ab01bcf4be7c8a06e4453ba5d811a0ea7
scottcwang <wangsc@cs.wisc.edu>
2020-07-06 14:34:45 -0500

    Copy KDF into module

---
+++ openssh_key/kdf.py
@@ -0,0 +1,52 @@
+import abc
+
+import bcrypt
+
+from openssh_key.pascal_style_byte_stream import PascalStyleFormatInstruction
+
+
+class KDF(abc.ABC):  # pragma: no cover
+    @staticmethod
+    @abc.abstractmethod
+    def derive_key(options, passphrase):
+        pass
+
+
+class NoneKDF(KDF):
+    @staticmethod
+    def derive_key(options, passphrase):
+        return {
+            'cipher_key': b'',
+            'initialization_vector': b''
+        }
+
+
+class BcryptKDF(KDF):
+    @staticmethod
+    def derive_key(options, passphrase):
+        bcrypt_result = bcrypt.kdf(
+            password=passphrase.encode(),
+            salt=options['salt'],
+            desired_key_bytes=32+16,  # https://blog.rebased.pl/2020/03/24/basic-key-security.html
+            rounds=options['rounds']
+        )
+        return {
+            'cipher_key': bcrypt_result[:32],
+            'initialization_vector': bcrypt_result[-16:]
+        }
+
+
+_kdf = {
+    'none': {
+        'kdf': NoneKDF,
+        'options_format': {'': '0s'}
+    },
+    'bcrypt': {
+        'kdf': BcryptKDF,
+        'options_format': {
+            'salt': PascalStyleFormatInstruction.BYTES,
+            'rounds': '>I'
+        }
+    }
+}
+
