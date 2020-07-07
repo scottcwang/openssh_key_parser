9d1d69922bd62ec704a03067900ad3375537b97b
scottcwang <wangsc@cs.wisc.edu>
2020-07-07 13:08:24 -0500

    Add tests for bcrypt kdf

--- tests/test_kdf.py
+++ tests/test_kdf.py
@@ -1,6 +1,7 @@
 import base64
 
 import pytest
+import bcrypt
 
 from openssh_key.kdf import create_kdf, NoneKDF, BcryptKDF
 
@@ -20,3 +21,36 @@ def test_none():
         'initialization_vector': b''
     }
 
+
+def test_bcrypt_calls_lib(mocker):
+    mocker.patch('bcrypt.kdf')
+
+    passphrase = 'abcd'
+    options = {
+        'salt': b'\x00',
+        'rounds': 1
+    }
+    BcryptKDF.derive_key(options, passphrase)
+    bcrypt.kdf.assert_called_once_with(
+        password=passphrase.encode(),
+        salt=options['salt'],
+        desired_key_bytes=BcryptKDF.IV_LENGTH+BcryptKDF.KEY_LENGTH,
+        rounds=options['rounds']
+    )
+
+
+def test_bcrypt_returns_key_iv(mocker):
+    def mock_bcrypt_kdf(password, salt, desired_key_bytes, rounds):
+        return b'\x00' * BcryptKDF.KEY_LENGTH + b'\x01' * BcryptKDF.IV_LENGTH
+    mocker.patch('bcrypt.kdf', mock_bcrypt_kdf)
+
+    passphrase = 'abcd'
+    options = {
+        'salt': b'\x00',
+        'rounds': 1
+    }
+    key_iv = BcryptKDF.derive_key(options, passphrase)
+    assert key_iv == {
+        'cipher_key': b'\x00' * BcryptKDF.KEY_LENGTH,
+        'initialization_vector': b'\x01' * BcryptKDF.IV_LENGTH
+    }
