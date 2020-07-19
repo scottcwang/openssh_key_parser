ada80745a88b237d38e471e33ddd764389aaee06
scottcwang <wangsc@cs.wisc.edu>
2020-07-18 23:29:58 -0500

    Add test case for one private key

--- tests/test_key_file.py
+++ tests/test_key_file.py
@@ -1,4 +1,5 @@
 import secrets
+import getpass
 
 import pytest
 
@@ -175,3 +176,135 @@ def test_private_key_list_negative_num_keys():
     byte_stream = PascalStyleByteStream(write_byte_stream.getvalue())
     with pytest.raises(ValueError):
         PrivateKeyList.from_byte_stream(byte_stream)
+
+
+def test_private_key_list_one_key(mocker):
+    write_byte_stream = PascalStyleByteStream()
+    header = {
+        'auth_magic': b'openssh-key-v1\x00',
+        'cipher': 'none',
+        'kdf': 'none',
+        'kdf_options': b'',
+        'num_keys': 1
+    }
+    write_byte_stream.write_from_format_instructions_dict(
+        {
+            'auth_magic': '15s',
+            'cipher': PascalStyleFormatInstruction.STRING,
+            'kdf': PascalStyleFormatInstruction.STRING,
+            'kdf_options': PascalStyleFormatInstruction.BYTES,
+            'num_keys': '>i'
+        },
+        header
+    )
+
+    public_bytes = secrets.token_bytes(ED25519_KEY_SIZE)
+
+    public_key_write_byte_stream = PascalStyleByteStream()
+    public_key_header = {
+        'key_type': 'ssh-ed25519'
+    }
+    public_key_write_byte_stream.write_from_format_instructions_dict(
+        {
+            'key_type': PascalStyleFormatInstruction.STRING
+        },
+        public_key_header
+    )
+    public_key_params = {
+        'public': public_bytes
+    }
+    public_key_write_byte_stream.write_from_format_instructions_dict(
+        Ed25519PublicKeyParams.public_format_instructions_dict(),
+        public_key_params
+    )
+    public_key_bytes = public_key_write_byte_stream.getvalue()
+    write_byte_stream.write_from_format_instruction(
+        PascalStyleFormatInstruction.BYTES,
+        public_key_bytes
+    )
+    public_key = PublicKey(PascalStyleByteStream(public_key_bytes))
+
+    private_key_write_byte_stream = PascalStyleByteStream()
+    private_key_header = {
+        'key_type': 'ssh-ed25519'
+    }
+    private_key_write_byte_stream.write_from_format_instructions_dict(
+        {
+            'key_type': PascalStyleFormatInstruction.STRING
+        },
+        private_key_header
+    )
+    private_key_params = {
+        'public': public_bytes,
+        'private_public': secrets.token_bytes(ED25519_KEY_SIZE) + public_bytes
+    }
+    private_key_write_byte_stream.write_from_format_instructions_dict(
+        Ed25519PrivateKeyParams.private_format_instructions_dict(),
+        private_key_params
+    )
+    private_key_footer = {
+        'comment': 'comment'
+    }
+    private_key_write_byte_stream.write_from_format_instructions_dict(
+        {
+            'comment': PascalStyleFormatInstruction.STRING
+        },
+        private_key_footer
+    )
+    private_key_bytes = private_key_write_byte_stream.getvalue()
+    private_key = PrivateKey(PascalStyleByteStream(private_key_bytes))
+
+    decipher_byte_stream = PascalStyleByteStream()
+    check_int = secrets.randbits(32)
+    decipher_bytes_header = {
+        'check_int_1': check_int,
+        'check_int_2': check_int
+    }
+    decipher_byte_stream.write_from_format_instructions_dict(
+        {
+            'check_int_1': '>I',
+            'check_int_2': '>I'
+        }, decipher_bytes_header
+    )
+    decipher_byte_stream.write(private_key_bytes)
+
+    passphrase = ''
+    kdf_result = NoneKDF.derive_key({}, '')
+
+    cipher_bytes = NoneCipher.encrypt(
+        kdf_result['cipher_key'],
+        kdf_result['initialization_vector'],
+        decipher_byte_stream.getvalue()
+    )
+
+    write_byte_stream.write_from_format_instruction(
+        PascalStyleFormatInstruction.BYTES,
+        cipher_bytes
+    )
+
+    byte_stream = PascalStyleByteStream(write_byte_stream.getvalue())
+
+    mocker.patch.object(getpass, 'getpass', return_value=passphrase)
+
+    private_key_list = PrivateKeyList.from_byte_stream(byte_stream)
+
+    getpass.getpass.assert_called_once()
+
+    assert private_key_list.bytes == byte_stream.getvalue()
+    assert private_key_list.header == header
+    assert private_key_list.cipher_bytes == cipher_bytes
+
+    assert private_key_list[0].public.header == public_key.header
+    assert private_key_list[0].public.params == public_key.params
+    assert private_key_list[0].public.footer == public_key.footer
+
+    assert private_key_list[0].private.header == private_key.header
+    assert private_key_list[0].private.params == private_key.params
+    assert private_key_list[0].private.footer == private_key.footer
+
+    assert private_key_list.kdf_options == {}  # TODO expand into function
+
+    assert private_key_list.decipher_bytes == decipher_byte_stream.getvalue()
+    assert private_key_list.decipher_bytes_header == decipher_bytes_header
+
+    assert private_key_list.decipher_padding == b''
