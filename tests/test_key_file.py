b3be176a7d32d60336ce23ed37df0e582b4b914b
scottcwang <wangsc@cs.wisc.edu>
2020-07-29 14:36:10 -0500

    Add test for multiple public keys

--- tests/test_key_file.py
+++ tests/test_key_file.py
@@ -9,14 +9,23 @@ from tests.test_key_list import (
     ED25519_TEST_HEADER,
     ED25519_TEST_PUBLIC,
     ED25519_TEST_PRIVATE,
-    PRIVATE_TEST_FOOTER
+    PRIVATE_TEST_FOOTER,
+    RSA_TEST_HEADER,
+    RSA_TEST_PUBLIC
 )
 
-PUBLIC_KEY_TEST = PublicKey(
-    ED25519_TEST_HEADER,
-    ED25519_TEST_PUBLIC,
-    {}
-)
+PUBLIC_KEY_TEST = [
+    PublicKey(
+        ED25519_TEST_HEADER,
+        ED25519_TEST_PUBLIC,
+        {}
+    ),
+    PublicKey(
+        RSA_TEST_HEADER,
+        RSA_TEST_PUBLIC,
+        {}
+    )
+]
 
 PRIVATE_KEYS_TEST = PrivateKeyList.from_list(
     [
@@ -55,13 +64,33 @@ def test_parse_key_container_private():
 
 def test_parse_key_container_public_one_key():
     comment = 'comment'
-    public_key_bytes = PUBLIC_KEY_TEST.pack_public()
+    public_key_bytes = PUBLIC_KEY_TEST[0].pack_public()
     public_key_b64 = base64.b64encode(public_key_bytes).decode()
-    public_key_string = PUBLIC_KEY_TEST.header['key_type'] + ' ' + \
+    public_key_string = PUBLIC_KEY_TEST[0].header['key_type'] + ' ' + \
         public_key_b64 + ' ' + \
-        comment
+        comment + '\n'
     key_container = kf.parse_key_container(public_key_string)
-    assert key_container[0] == PUBLIC_KEY_TEST
+    assert key_container[0] == PUBLIC_KEY_TEST[0]
     assert key_container[0].key_type_clear == \
-        PUBLIC_KEY_TEST.header['key_type']
+        PUBLIC_KEY_TEST[0].header['key_type']
     assert key_container[0].comment_clear == comment
+
+
+def test_parse_key_container_public_two_keys():
+    public_key_string = ''
+
+    for i, public_key in enumerate(PUBLIC_KEY_TEST):
+        comment = f'comment_{i}'
+        public_key_bytes = public_key.pack_public()
+        public_key_b64 = base64.b64encode(public_key_bytes).decode()
+        public_key_string += public_key.header['key_type'] + ' ' + \
+            public_key_b64 + ' ' + \
+            comment + '\n'
+
+    key_container = kf.parse_key_container(public_key_string)
+
+    for i, public_key_parsed in enumerate(key_container):
+        assert key_container[i] == public_key_parsed
+        assert key_container[i].key_type_clear == \
+            public_key_parsed.header['key_type']
+        assert key_container[i].comment_clear == f'comment_{i}'
