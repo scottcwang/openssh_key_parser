a2516ecced4acb1391e7ddf01ef301acb5dfc5ec
scottcwang <wangsc@cs.wisc.edu>
2020-08-09 14:43:22 -0500

    Add documentation from type hints

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ author = 'Scott C Wang'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
