fb36c5c747adf2c04fc3e0931350e20cf86bafd9
scottcwang <wangsc@cs.wisc.edu>
2020-08-09 18:47:07 -0500

    Add docstrings to cipher module

--- openssh_key/cipher.py
+++ openssh_key/cipher.py
@@ -1,3 +1,8 @@
+"""Classes representing symmetric-key ciphers.
+
+The abstract base class is :py:class:`Cipher`.
+"""
+
 import abc
 import typing
 
@@ -8,6 +13,12 @@ from cryptography.hazmat.backends import default_backend
 
 
 class Cipher(abc.ABC):
+    """An abstract symmetric-key cipher.
+
+    Used to encrypt and decrypt private plaintext bytes of a length that is a
+    multiple of a cipher-specific block size, given a key and an
+    initialization vector.
+    """
     @staticmethod
     @abc.abstractmethod
     def encrypt(
@@ -15,7 +26,20 @@ class Cipher(abc.ABC):
         initialization_vector: bytes,
         plain_bytes: bytes
     ) -> bytes:
-        pass
+        """Encrypts the given plaintext bytes using the given key and
+        initialization vector.
+
+        Args:
+            cipher_key
+                Symmetric key.
+            initialization_vector
+                Cipher initialization vector.
+            plain_bytes
+                Plaintext bytes to be encrypted.
+
+        Returns:
+            Ciphertext bytes.
+        """
 
     @staticmethod
     @abc.abstractmethod
@@ -24,21 +48,50 @@ class Cipher(abc.ABC):
         initialization_vector: bytes,
         cipher_bytes: bytes
     ) -> bytes:
-        pass
+        """Decrypts the given ciphertext bytes using the given key and
+        initialization vector.
+
+        Args:
+            cipher_key
+                Symmetric key.
+            initialization_vector
+                Cipher initialization vector.
+            cipher_bytes
+                Ciphertext bytes to be decrypted.
+
+        Returns:
+            Plaintext bytes.
+        """
 
     @staticmethod
     @abc.abstractmethod
     def block_size() -> int:
-        pass
+        """The block size for this cipher.
+        """
 
 
 class NoneCipher(Cipher):
+    """Null encryption.
+    """
     @staticmethod
     def encrypt(
         cipher_key: bytes,
         initialization_vector: bytes,
         plain_bytes: bytes
     ) -> bytes:
+        """Returns the plaintext bytes as given.
+
+        Args:
+            cipher_key
+                Ignored.
+            initialization_vector
+                Ignored.
+            plain_bytes
+                Plaintext bytes to be returned.
+
+        Returns:
+            The given plaintext bytes.
+        """
         return plain_bytes
 
     @staticmethod
@@ -47,22 +100,63 @@ class NoneCipher(Cipher):
         initialization_vector: bytes,
         cipher_bytes: bytes
     ) -> bytes:
+        """Returns the ciphertext bytes as given.
+
+        Args:
+            cipher_key
+                Ignored.
+            initialization_vector
+                Ignored.
+            cipher_bytes
+                Ciphertext bytes to be returned.
+
+        Returns:
+            The given ciphertext bytes.
+        """
         return cipher_bytes
 
     BLOCK_SIZE = 8
 
     @staticmethod
     def block_size() -> int:
+        """The value 8, the cipher block size OpenSSH uses to pad private
+        bytes under null encryption.
+
+        Notes:
+            https://github.com/openssh/openssh-portable/blob/9cd40b829a5295cc81fbea8c7d632b2478db6274/cipher.c#L112
+        """
         return NoneCipher.BLOCK_SIZE
 
 
 class AES256_CTRCipher(Cipher):
+    """The Advanced Encryption Standard (the Rijndael block cipher) with a key
+    length of 256 bits, under the counter mode of operation initialized with a
+    given initialization vector.
+    """
     @staticmethod
     def encrypt(
         cipher_key: bytes,
         initialization_vector: bytes,
         plain_bytes: bytes
     ) -> bytes:
+        """Encrypts the given plaintext bytes using the given key and
+        initialization vector.
+
+        Args:
+            cipher_key
+                Symmetric key.
+            initialization_vector
+                Cipher initialization vector.
+            plain_bytes
+                Plaintext bytes to be encrypted.
+
+        Raises:
+            ValueError: The key length is not one of 128, 192, 256, 512 bits,
+                or the initialization vector is not of length 128 bits.
+
+        Returns:
+            Ciphertext bytes.
+        """
         cipher = ciphers.Cipher(
             algorithms.AES(cipher_key),
             modes.CTR(initialization_vector),
@@ -77,6 +171,24 @@ class AES256_CTRCipher(Cipher):
         initialization_vector: bytes,
         cipher_bytes: bytes
     ) -> bytes:
+        """Decrypts the given ciphertext bytes using the given key and
+        initialization vector.
+
+        Args:
+            cipher_key
+                Symmetric key.
+            initialization_vector
+                Cipher initialization vector.
+            cipher_bytes
+                Ciphertext bytes to be decrypted.
+
+        Raises:
+            ValueError: The key length is not one of 128, 192, 256, 512 bits,
+                or the initialization vector is not of length 128 bits.
+
+        Returns:
+            Plaintext bytes.
+        """
         cipher = ciphers.Cipher(
             algorithms.AES(cipher_key),
             modes.CTR(initialization_vector),
@@ -89,6 +201,8 @@ class AES256_CTRCipher(Cipher):
 
     @staticmethod
     def block_size() -> int:
+        """The value 16, the cipher block size of AES.
+        """
         return AES256_CTRCipher.BLOCK_SIZE
 
 
@@ -99,4 +213,18 @@ _CIPHER_MAPPING = {
 
 
 def create_cipher(cipher_type: str) -> typing.Type[Cipher]:
+    """Returns the class corresponding to the given cipher type name.
+
+    Args:
+        cipher_type
+            The name of the OpenSSH private key cipher type.
+
+    Returns:
+        The subclass of :py:class:`Cipher` corresponding to the cipher type
+        name.
+
+    Raises:
+        KeyError: There is no subclass of :py:class:`Cipher` corresponding to
+            the given cipher type name.
+    """
     return _CIPHER_MAPPING[cipher_type]
