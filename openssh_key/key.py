6dd5e695687e5fd9f45f9180fc8bee96fc0440e6
scottcwang <wangsc@cs.wisc.edu>
2020-08-11 20:38:08 -0500

    Add docstrings to key module

--- openssh_key/key.py
+++ openssh_key/key.py
@@ -1,3 +1,7 @@
+"""Classes representing containers for serializing and deserializing
+:any:`PublicKeyParams` objects.
+"""
+
 import warnings
 import base64
 import typing
@@ -25,14 +29,39 @@ KeyTypeVar = typing.TypeVar(
 
 
 class Key(typing.Generic[PublicKeyParamsTypeVar]):
+    """A container for a :any:`PublicKeyParams`, an encoded header and footer,
+    and cleartext key details.
+
+    Args:
+        header
+            A :any:`typing.Mapping` with the contents of the encoded header.
+        params
+            A :any:`typing.Mapping` with the parameter values.
+        footer
+            A :any:`typing.Mapping` with the contents of the encoded header.
+        clear
+            A :any:`typing.Mapping` with cleartext key details, if any.
+    """
     @staticmethod
     def header_format_instructions_dict() -> FormatInstructionsDict:
+        """The Pascal-style byte stream format instructions for the encoded
+        header.
+
+        Returns:
+            The format instructions.
+        """
         return {
             'key_type': PascalStyleFormatInstruction.STRING
         }
 
     @staticmethod
     def footer_format_instructions_dict() -> FormatInstructionsDict:
+        """The Pascal-style byte stream format instructions for the encoded
+        footer.
+
+        Returns:
+            The format instructions.
+        """
         return {}
 
     @staticmethod
@@ -40,6 +69,17 @@ class Key(typing.Generic[PublicKeyParamsTypeVar]):
         key_type: str,
         byte_stream: PascalStyleByteStream
     ) -> ValuesDict:
+        """Reads parameter values from a given bytestream for a given key type.
+
+        Args:
+            key_type
+                The key type name.
+            byte_stream
+                The bytestream from which to read.
+
+        Returns:
+            A :any:`typing.Mapping` containing the read parameter values.
+        """
         return byte_stream.read_from_format_instructions_dict(
             create_public_key_params(
                 key_type
@@ -52,7 +92,19 @@ class Key(typing.Generic[PublicKeyParamsTypeVar]):
         key_type: str,
         key_params_dict: ValuesDict
     ) -> PublicKeyParamsTypeVar:
-        pass
+        """Converts a :any:`typing.Mapping` of parameter values to a
+        :any:`PublicKeyParams` object of a given key type.
+
+        Args:
+            key_type
+                The key type name.
+            key_params_dict
+                The parameter values.
+
+        Returns:
+            A :any:`PublicKeyParams` containing the parameter values in
+            ``key_params_dict``.
+        """
 
     def __init__(
         self,
@@ -84,6 +136,19 @@ class Key(typing.Generic[PublicKeyParamsTypeVar]):
         byte_stream: PascalStyleByteStream,
         clear: typing.Optional[ValuesDict] = None
     ) -> KeyTypeVar:
+        """Reads parameter values, encoded header, and encoded footer from a
+        given bytestream.
+
+        Args:
+            byte_stream
+                The bytestream from which to read.
+            clear
+                Cleartext key details to add to the returned object.
+
+        Returns:
+            A :any:`Key` object containing the parameter values, encoded
+            header, encoded footer, and cleartext key details.
+        """
         header = byte_stream.read_from_format_instructions_dict(
             cls.header_format_instructions_dict()
         )
@@ -105,6 +170,23 @@ class Key(typing.Generic[PublicKeyParamsTypeVar]):
         byte_string: bytes,
         clear: typing.Optional[ValuesDict] = None
     ) -> KeyTypeVar:
+        """Parses parameter values, encoded header, and encoded footer from a
+        given byte string.
+
+        Args:
+            byte_string
+                The byte string from which to parse.
+            clear
+                Cleartext key details to add to the returned object.
+
+        Returns:
+            A :any:`Key` object containing the parameter values, encoded
+            header, encoded footer, and cleartext key details.
+
+        Raises:
+            UserWarning: There are additional bytes in the encoded key than
+                would be expected from the key count.
+        """
         byte_stream = PascalStyleByteStream(byte_string)
 
         key = cls.from_byte_stream(byte_stream, clear)
@@ -121,6 +203,23 @@ class Key(typing.Generic[PublicKeyParamsTypeVar]):
         cls: typing.Type[KeyTypeVar],
         string: str
     ) -> KeyTypeVar:
+        """Parses parameter values, encoded header, encoded footer, and
+        cleartext key details from a given string in OpenSSH `public key format
+        <https://github.com/openssh/openssh-portable/blob/ed6bef77f5bb5b8f9ca2914478949e29f2f0a780/PROTOCOL#L470>`_.
+
+        Args:
+            string
+                The string from which to parse.
+
+        Returns:
+            A :any:`Key` object containing the parameter values, encoded
+            header, encoded footer, and cleartext key details.
+
+        Raises:
+            UserWarning: The cleartext key type and the encoded key type do not
+                match, or there are additional bytes in the encoded key than
+                would be expected from the key count.
+        """
         key_type_clear, key_b64, comment_clear = string.split(' ', maxsplit=2)
         key_bytes = base64.b64decode(key_b64)
         public_key = cls.from_bytes(
@@ -137,6 +236,13 @@ class Key(typing.Generic[PublicKeyParamsTypeVar]):
         return public_key
 
     def pack_public_bytes(self) -> bytes:
+        """Packs the public parameter values, encoded header, and encoded
+        footer into a byte string.
+
+        Returns:
+            A byte string containing the public parameter values, encoded
+            header, and encoded footer.
+        """
         key_byte_stream = PascalStyleByteStream()
 
         key_byte_stream.write_from_format_instructions_dict(
@@ -145,7 +251,7 @@ class Key(typing.Generic[PublicKeyParamsTypeVar]):
         )
 
         self_params_types: typing.Sequence[typing.Type[PublicKeyParams]] = \
-             type(self.params).mro()
+            type(self.params).mro()
         first_public_key_params_type = next(
             (
                 params_type for params_type in self_params_types
@@ -171,6 +277,23 @@ class Key(typing.Generic[PublicKeyParamsTypeVar]):
         use_footer_comment: bool = True,
         use_clear_comment: bool = True
     ) -> str:
+        """Packs the parameter values, encoded header, encoded footer, and
+        cleartext key details into a string in OpenSSH `public key format
+        <https://github.com/openssh/openssh-portable/blob/ed6bef77f5bb5b8f9ca2914478949e29f2f0a780/PROTOCOL#L470>`_.
+
+        Args:
+            use_footer_comment
+                Append the comment in the footer, if any, as cleartext to the
+                encoded string.
+            use_clear_comment
+                Append the comment in the cleartext key details, if any, to the
+                encoded string (if ``use_footer_comment`` is true, appends
+                after the comment in the footer).
+
+        Returns:
+            A string containing the parameter values, encoded header, encoded
+            footer, and cleartext key details.
+        """
         text = str(self.header['key_type']) + ' '
 
         public_key_bytes = self.pack_public_bytes()
@@ -233,9 +356,29 @@ class PrivateKey(Key[PrivateKeyParams]):
         key_type: str,
         key_params_dict: ValuesDict
     ) -> PrivateKeyParams:
+        """Converts a :any:`typing.Mapping` of parameter values to a
+        :any:`PrivateKeyParams` object of a given key type.
+
+        Args:
+            key_type
+                The key type name.
+            key_params_dict
+                The parameter values.
+
+        Returns:
+            A :any:`PrivateKeyParams` containing the parameter values in
+            ``key_params_dict``.
+        """
         return create_private_key_params(key_type)(key_params_dict)
 
     def pack_private_bytes(self) -> bytes:
+        """Packs the private parameter values, encoded header, and encoded
+        footer into a byte string.
+
+        Returns:
+            A byte string containing the private parameter values, encoded
+            header, and encoded footer.
+        """
         key_byte_stream = PascalStyleByteStream()
 
         key_byte_stream.write_from_format_instructions_dict(
