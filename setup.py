bb8c3322f5d4ff8b9068463c56453ec726fc2474
scottcwang <wangsc@cs.wisc.edu>
2020-08-12 21:34:54 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openssh_key_parser",
+    version="0.0.1",
+    author="Scott C Wang",
+    author_email="wangsc@cs.wisc.edu",
+    description="Parse and pack OpenSSH private and public key files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scottcwang/openssh_key_parser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8'
+)
