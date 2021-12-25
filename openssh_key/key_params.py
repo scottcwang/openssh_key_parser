a601e197512aa7d5fdf86047ba57144c3acd9d11
scottcwang <wangsc@cs.wisc.edu>
2021-12-25 22:23:00 +0000

    Support ssh-dss keys

--- openssh_key/key_params.py
+++ openssh_key/key_params.py
@@ -11,7 +11,8 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import (
     rsa,
-    ed25519
+    ed25519,
+    dsa
 )
 
 from openssh_key.pascal_style_byte_stream import (
@@ -785,6 +786,213 @@ class Ed25519PrivateKeyParams(PrivateKeyParams, Ed25519PublicKeyParams):
         return conversion_functions_dict
 
 
+class DSSPublicKeyParams(PublicKeyParams):
+    """The parameters comprising a key in the Digital Signature Standard
+    cryptosystem (FIPS 186).
+
+    The names and iteration order of parameters of a *public* DSS key is:
+
+    * ``p``: The public modulus (:any:`int`).
+    * ``q``: The subgroup order (:any:`int`).
+    * ``g``: The generator (:any:`int`).
+    * ``y``: The public key (:any:`int`).
+
+    NB: OpenSSH `has deprecated <https://www.openssh.com/legacy.html>`_ the
+    "ssh-dss" public key algorithm. OpenSSH only supports DSS keys of length
+    1024 bits, which are vulnerable to the
+    `Logjam attack <https://weakdh.org/>`_.
+
+    Args:
+        params
+            The values with which to initialize this parameters object. All
+            given values are saved, even those that do not exist in the format
+            instructions for this key type.
+
+    Raises:
+        UserWarning: A parameter value from the above list is missing from
+            ``params`` or does not have the correct type.
+    """
+    FORMAT_INSTRUCTIONS_DICT: typing.ClassVar[FormatInstructionsDict] = {
+        'p': PascalStyleFormatInstruction.MPINT,
+        'q': PascalStyleFormatInstruction.MPINT,
+        'g': PascalStyleFormatInstruction.MPINT,
+        'y': PascalStyleFormatInstruction.MPINT,
+    }
+
+    @staticmethod
+    def conversion_functions(
+    ) -> typing.Mapping[
+        typing.Type[typing.Any],
+        ConversionFunctions
+    ]:
+        """Conversion functions for key objects of the following types:
+
+        * :any:`cryptography.hazmat.primitives.asymmetric.dsa.DSAPublicKey`
+
+        Returns:
+            A :py:class:`typing.Mapping` from the above types of key objects
+            to functions that take key objects of these types and return
+            parameter values.
+        """
+        def dsa_public_key_convert_from_cryptography(
+            key_object: dsa.DSAPublicKey
+        ) -> ValuesDict:
+            public_numbers = key_object.public_numbers()
+            parameter_numbers = public_numbers.parameter_numbers
+            return {
+                'p': parameter_numbers.p,
+                'q': parameter_numbers.q,
+                'g': parameter_numbers.g,
+                'y': public_numbers.y,
+            }
+
+        def dsa_public_key_convert_to_cryptography(
+            key_params: ValuesDict
+        ) -> dsa.DSAPublicKey:
+            return dsa.DSAPublicNumbers(
+                key_params['y'],
+                dsa.DSAParameterNumbers(
+                    key_params['p'],
+                    key_params['q'],
+                    key_params['g']
+                )
+            ).public_key(default_backend())
+
+        return {
+            dsa.DSAPublicKey: ConversionFunctions(
+                dsa_public_key_convert_from_cryptography,
+                dsa_public_key_convert_to_cryptography
+            )
+        }
+
+
+DSSPrivateKeyParamsTypeVar = typing.TypeVar(
+    'DSSPrivateKeyParamsTypeVar',
+    bound='DSSPrivateKeyParams'
+)
+
+
+class DSSPrivateKeyParams(PrivateKeyParams, DSSPublicKeyParams):
+    """The parameters comprising a private key in the Digital Signature Standard
+    cryptosystem (FIPS 186).
+
+    The names and iteration order of parameters of a *private* DSS key is:
+
+    * ``p``: The public modulus (:any:`int`).
+    * ``q``: The subgroup order (:any:`int`).
+    * ``g``: The generator (:any:`int`).
+    * ``y``: The public key (:any:`int`).
+    * ``x``: The private key (:any:`int`).
+
+    NB: OpenSSH `has deprecated <https://www.openssh.com/legacy.html>`_ the
+    "ssh-dss" public key algorithm. OpenSSH only supports DSS keys of length
+    1024 bits, which are vulnerable to the
+    `Logjam attack <https://weakdh.org/>`_.
+
+    Args:
+        params
+            The values with which to initialize this parameters object. All
+            given values are saved, even those that do not exist in the format
+            instructions for this key type.
+
+    Raises:
+        UserWarning: A parameter value from the above list is missing from
+            ``params`` or does not have the correct type.
+    """
+
+    FORMAT_INSTRUCTIONS_DICT: typing.ClassVar[FormatInstructionsDict] = {
+        'p': PascalStyleFormatInstruction.MPINT,
+        'q': PascalStyleFormatInstruction.MPINT,
+        'g': PascalStyleFormatInstruction.MPINT,
+        'y': PascalStyleFormatInstruction.MPINT,
+        'x': PascalStyleFormatInstruction.MPINT,
+    }
+
+    KEY_SIZE = 1024
+
+    @classmethod
+    def generate_private_params(
+        cls: typing.Type[DSSPrivateKeyParamsTypeVar],
+        **kwargs: typing.Any
+    ) -> DSSPrivateKeyParamsTypeVar:
+        """Constructs and initializes a DSS private key parameters object
+        with generated values.
+
+        Args:
+            kwargs
+                Keyword arguments consumed to generate parameter values.
+
+        Returns:
+            A private key parameters object with generated values valid for
+            a DSS private key (the key size is 128 bytes).
+        """
+
+        private_key = dsa.generate_private_key(DSSPrivateKeyParams.KEY_SIZE)
+
+        private_numbers = private_key.private_numbers()
+        public_numbers = private_numbers.public_numbers
+        parameter_numbers = public_numbers.parameter_numbers
+
+        return cls({
+            'p': parameter_numbers.p,
+            'q': parameter_numbers.q,
+            'g': parameter_numbers.g,
+            'y': public_numbers.y,
+            'x': private_numbers.x,
+        })
+
+    @staticmethod
+    def conversion_functions(
+    ) -> typing.Mapping[
+        typing.Type[typing.Any],
+        ConversionFunctions
+    ]:
+        """Conversion functions for key objects of the following types:
+
+        * :any:`cryptography.hazmat.primitives.asymmetric.dsa.DSAPrivateKey`
+
+        Returns:
+            A :py:class:`typing.Mapping` from the above types of key objects
+            to functions that take key objects of these types and return
+            parameter values.
+        """
+        def dsa_private_key_convert_from_cryptography(
+            key_object: dsa.DSAPrivateKey
+        ) -> ValuesDict:
+            private_numbers = key_object.private_numbers()
+            public_numbers = private_numbers.public_numbers
+            parameter_numbers = public_numbers.parameter_numbers
+            return {
+                'p': parameter_numbers.p,
+                'q': parameter_numbers.q,
+                'g': parameter_numbers.g,
+                'y': public_numbers.y,
+                'x': private_numbers.x,
+            }
+
+        def dsa_private_key_convert_to_cryptography(
+            key_params: ValuesDict
+        ) -> dsa.DSAPrivateKey:
+            return dsa.DSAPrivateNumbers(
+                key_params['x'],
+                dsa.DSAPublicNumbers(
+                    key_params['y'],
+                    dsa.DSAParameterNumbers(
+                        key_params['p'],
+                        key_params['q'],
+                        key_params['g']
+                    )
+                )
+            ).private_key(default_backend())
+
+        return {
+            dsa.DSAPrivateKey: ConversionFunctions(
+                dsa_private_key_convert_from_cryptography,
+                dsa_private_key_convert_to_cryptography
+            )
+        }
+
+
 class PublicPrivateKeyParamsClasses(typing.NamedTuple):
     publicKeyParamsClass: typing.Type[PublicKeyParams]
     privateKeyParamsClass: typing.Type[PrivateKeyParams]
@@ -797,6 +1005,9 @@ _KEY_TYPE_MAPPING = {
     'ssh-ed25519': PublicPrivateKeyParamsClasses(
         Ed25519PublicKeyParams, Ed25519PrivateKeyParams
     ),
+    'ssh-dss': PublicPrivateKeyParamsClasses(
+        DSSPublicKeyParams, DSSPrivateKeyParams
+    )
 }
 
 
--- tests/test_key_params.py
+++ tests/test_key_params.py
@@ -7,7 +7,8 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import (
     rsa,
-    ed25519
+    ed25519,
+    dsa
 )
 import nacl.signing
 
@@ -17,7 +18,9 @@ from openssh_key.key_params import (
     RSAPublicKeyParams,
     RSAPrivateKeyParams,
     Ed25519PublicKeyParams,
-    Ed25519PrivateKeyParams
+    Ed25519PrivateKeyParams,
+    DSSPublicKeyParams,
+    DSSPrivateKeyParams
 )
 from openssh_key.pascal_style_byte_stream import PascalStyleFormatInstruction
 
@@ -772,6 +775,325 @@ def test_ed25519_public_convert_to_not_implemented():
     ed25519_private = Ed25519PrivateKeyParams.generate_private_params()
     with pytest.raises(NotImplementedError):
         assert ed25519_private.convert_to(type)
+
+
+def test_factory_dss_public():
+    assert create_public_key_params('ssh-dss') == DSSPublicKeyParams
+
+
+def test_factory_dss_private():
+    assert create_private_key_params('ssh-dss') == DSSPrivateKeyParams
+
+
+def test_dss_public_format_instructions_dict():
+    assert DSSPublicKeyParams.FORMAT_INSTRUCTIONS_DICT == {
+        'p': PascalStyleFormatInstruction.MPINT,
+        'q': PascalStyleFormatInstruction.MPINT,
+        'g': PascalStyleFormatInstruction.MPINT,
+        'y': PascalStyleFormatInstruction.MPINT,
+    }
+
+
+def test_dss_private_format_instructions_dict():
+    assert DSSPrivateKeyParams.FORMAT_INSTRUCTIONS_DICT == {
+        'p': PascalStyleFormatInstruction.MPINT,
+        'q': PascalStyleFormatInstruction.MPINT,
+        'g': PascalStyleFormatInstruction.MPINT,
+        'y': PascalStyleFormatInstruction.MPINT,
+        'x': PascalStyleFormatInstruction.MPINT,
+    }
+
+
+def test_dss_public_check_params_are_valid():
+    dss_public = DSSPublicKeyParams({
+        'p': 1,
+        'q': 2,
+        'g': 3,
+        'y': 4,
+    })
+    with pytest.warns(None) as warnings_list:
+        dss_public.check_params_are_valid()
+    assert not warnings_list
+
+
+def test_dss_public_check_extra_params_are_valid():
+    dss_public = DSSPublicKeyParams({
+        'p': 1,
+        'q': 2,
+        'g': 3,
+        'y': 4,
+        'random': 5
+    })
+    with pytest.warns(None) as warnings_list:
+        dss_public.check_params_are_valid()
+    assert not warnings_list
+
+
+def test_dss_public_missing_params_are_not_valid():
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        dss_public = DSSPublicKeyParams({
+            'p': 1,
+            'q': 2,
+            'g': 3,
+        })
+    with pytest.warns(UserWarning, match='y missing'):
+        dss_public.check_params_are_valid()
+
+
+def test_dss_public_bad_type_params_are_not_valid():
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        dss_public = DSSPublicKeyParams({
+            'p': 1,
+            'q': 2,
+            'g': 3,
+            'y': b'bad',
+        })
+    with pytest.warns(UserWarning, match='y should be of class int'):
+        dss_public.check_params_are_valid()
+
+
+def test_dss_private_check_params_are_valid():
+    dss_private = DSSPrivateKeyParams({
+        'p': 1,
+        'q': 2,
+        'g': 3,
+        'y': 4,
+        'x': 5,
+    })
+    with pytest.warns(None) as warnings_list:
+        dss_private.check_params_are_valid()
+    assert not warnings_list
+
+
+def test_dss_private_check_extra_params_are_valid():
+    dss_private = DSSPrivateKeyParams({
+        'p': 1,
+        'q': 2,
+        'g': 3,
+        'y': 4,
+        'x': 5,
+        'random': 6
+    })
+    with pytest.warns(None) as warnings_list:
+        dss_private.check_params_are_valid()
+    assert not warnings_list
+
+
+def test_dss_private_missing_params_are_not_valid():
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        dss_private = DSSPrivateKeyParams({
+            'p': 1,
+            'q': 2,
+            'g': 3,
+            'y': 4,
+        })
+    with pytest.warns(UserWarning, match='x missing'):
+        dss_private.check_params_are_valid()
+
+
+def test_dss_private_bad_type_params_are_not_valid():
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        dss_private = DSSPrivateKeyParams({
+            'p': 1,
+            'q': 2,
+            'g': 3,
+            'y': 4,
+            'x': b'bad'
+        })
+    with pytest.warns(UserWarning, match='x should be of class int'):
+        dss_private.check_params_are_valid()
+
+
+def test_dss_public():
+    dss_public_dict = {
+        'p': 1,
+        'q': 2,
+        'g': 3,
+        'y': 4,
+    }
+    dss_public = DSSPublicKeyParams(dss_public_dict)
+    assert dss_public.params == dss_public_dict
+
+
+def test_dss_public_missing_params():
+    with pytest.warns(UserWarning, match='y missing'):
+        DSSPublicKeyParams({
+            'p': 1,
+            'q': 2,
+            'g': 3,
+        })
+
+
+def test_dss_public_convert_from_unknown():
+    with pytest.raises(NotImplementedError):
+        DSSPublicKeyParams.convert_from('random')
+
+
+def test_dss_public_convert_from_cryptography_public():
+    private_key = dsa.generate_private_key(
+        DSSPrivateKeyParams.KEY_SIZE
+    ).public_key()
+    public_numbers = private_key.public_numbers()
+    parameter_numbers = public_numbers.parameter_numbers
+    converted = DSSPublicKeyParams.convert_from(private_key)
+    assert type(converted) == DSSPublicKeyParams
+    assert converted == {
+        'p': parameter_numbers.p,
+        'q': parameter_numbers.q,
+        'g': parameter_numbers.g,
+        'y': public_numbers.y,
+    }
+
+
+def test_dss_public_convert_from_cryptography_private():
+    private_key = dsa.generate_private_key(
+        DSSPrivateKeyParams.KEY_SIZE
+    )
+    private_numbers = private_key.private_numbers()
+    public_numbers = private_numbers.public_numbers
+    parameter_numbers = public_numbers.parameter_numbers
+    converted = DSSPublicKeyParams.convert_from(private_key)
+    assert type(converted) == DSSPublicKeyParams
+    assert converted == {
+        'p': parameter_numbers.p,
+        'q': parameter_numbers.q,
+        'g': parameter_numbers.g,
+        'y': public_numbers.y,
+    }
+
+
+def test_dss_public_convert_to_cryptography_public():
+    dss_private = DSSPrivateKeyParams.generate_private_params()
+    dss_public = DSSPublicKeyParams({
+        'p': dss_private['p'],
+        'q': dss_private['q'],
+        'g': dss_private['g'],
+        'y': dss_private['y'],
+    })
+    converted = dss_public.convert_to(dsa.DSAPublicKey)
+    assert isinstance(converted, dsa.DSAPublicKey)
+    assert converted.public_numbers() == dsa.DSAPublicNumbers(
+        dss_public['y'],
+        dsa.DSAParameterNumbers(
+            dss_public['p'],
+            dss_public['q'],
+            dss_public['g']
+        )
+    )
+
+
+def test_dss_private():
+    dss_private_dict = {
+        'p': 1,
+        'q': 2,
+        'g': 3,
+        'y': 4,
+        'x': 5,
+    }
+    dss_private = DSSPrivateKeyParams(dss_private_dict)
+    assert dss_private.params == dss_private_dict
+
+
+def test_dss_private_missing_params():
+    with pytest.warns(UserWarning, match='x missing'):
+        DSSPrivateKeyParams({
+            'p': 1,
+            'q': 2,
+            'g': 3,
+            'y': 4,
+        })
+
+
+def test_dss_private_bad_type_params():
+    with pytest.warns(UserWarning, match='x should be of class int'):
+        DSSPrivateKeyParams({
+            'p': 1,
+            'q': 2,
+            'g': 3,
+            'y': 4,
+            'x': b'bad'
+        })
+
+
+def test_dss_private_convert_from_unknown():
+    with pytest.raises(NotImplementedError):
+        DSSPrivateKeyParams.convert_from('random')
+
+
+def test_dss_private_convert_from_cryptography_private():
+    private_key = dsa.generate_private_key(
+        DSSPrivateKeyParams.KEY_SIZE
+    )
+    private_numbers = private_key.private_numbers()
+    public_numbers = private_numbers.public_numbers
+    parameter_numbers = public_numbers.parameter_numbers
+    converted = DSSPrivateKeyParams.convert_from(private_key)
+    assert type(converted) == DSSPrivateKeyParams
+    assert converted == {
+        'p': parameter_numbers.p,
+        'q': parameter_numbers.q,
+        'g': parameter_numbers.g,
+        'y': public_numbers.y,
+        'x': private_numbers.x,
+    }
+
+
+def test_dss_private_convert_to_cryptography_private():
+    dss_private = DSSPrivateKeyParams.generate_private_params()
+    converted = dss_private.convert_to(dsa.DSAPrivateKeyWithSerialization)
+    assert isinstance(converted, dsa.DSAPrivateKeyWithSerialization)
+    assert converted.private_numbers() == dsa.DSAPrivateNumbers(
+        dss_private['x'],
+        dsa.DSAPublicNumbers(
+            dss_private['y'],
+            dsa.DSAParameterNumbers(
+                dss_private['p'],
+                dss_private['q'],
+                dss_private['g']
+            )
+        )
+    )
+
+
+def test_dss_private_convert_to_cryptography_dssprivatekey():
+    dss_private = DSSPrivateKeyParams.generate_private_params()
+    converted = dss_private.convert_to(dsa.DSAPrivateKey)
+    assert isinstance(converted, dsa.DSAPrivateKey)
+    assert converted.private_numbers() == dsa.DSAPrivateNumbers(
+        dss_private['x'],
+        dsa.DSAPublicNumbers(
+            dss_private['y'],
+            dsa.DSAParameterNumbers(
+                dss_private['p'],
+                dss_private['q'],
+                dss_private['g']
+            )
+        )
+    )
+
+
+def test_dss_private_convert_to_cryptography_public():
+    dss_private = DSSPrivateKeyParams.generate_private_params()
+    converted = dss_private.convert_to(dsa.DSAPublicKey)
+    assert isinstance(converted, dsa.DSAPublicKey)
+    assert converted.public_numbers() == dsa.DSAPublicNumbers(
+        dss_private['y'],
+        dsa.DSAParameterNumbers(
+            dss_private['p'],
+            dss_private['q'],
+            dss_private['g']
+        )
+    )
+
+
+def test_dss_public_convert_to_not_implemented():
+    dss_private = DSSPrivateKeyParams.generate_private_params()
+    with pytest.raises(NotImplementedError):
+        assert dss_private.convert_to(type)
 
 
 def test_str():
