21adf7c193030e5f521f65d562cc351ddd0b3429
scottcwang <wangsc@cs.wisc.edu>
2022-01-03 01:35:43 +0000

    Explicitly state reexports of key_params package

--- openssh_key/key_params/__init__.py
+++ openssh_key/key_params/__init__.py
@@ -4,3 +4,19 @@ from .ecdsa import *
 from .ed25519 import *
 from .factory import *
 from .rsa import *
+
+__all__ = [
+    'PrivateKeyParams', 'PrivateKeyParamsTypeVar',
+    'PublicKeyParams', 'PublicKeyParamsTypeVar',
+    'DSSPrivateKeyParams', 'DSSPublicKeyParams',
+    'ECDSA_NISTP256_PrivateKeyParams',
+    'ECDSA_NISTP256_PublicKeyParams',
+    'ECDSA_NISTP384_PrivateKeyParams',
+    'ECDSA_NISTP384_PublicKeyParams',
+    'ECDSA_NISTP521_PrivateKeyParams',
+    'ECDSA_NISTP521_PublicKeyParams', 'ECDSAPrivateKeyParams',
+    'ECDSAPublicKeyParams',
+    'Ed25519PrivateKeyParams', 'Ed25519PublicKeyParams',
+    'RSAPrivateKeyParams', 'RSAPublicKeyParams',
+    'create_private_key_params', 'create_public_key_params'
+]
